0d128056a3086e6b7b03f7bcbca99456e6380870
Roman Krasiuk <rokrassyuk@gmail.com>
2019-01-11 14:13:58 +0200

    add roadmap to readme

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # cypher-artisan
+
+## Roadmap
+
+### Query Clauses List
+        
+    + Match
+    + Where
+    + With
+    + Return
+    + OrderBy
+    + Limit
+
+    - As
+    - OptionalMatch
+    - Skip
+    - Create
+    - CreateUnique
+    - Merge
+    - Set
+    - Delete
+    - Remove
+    - ForEach
+    - ReturnDistinct
+    - OrderByDesc
+    - Union
+    - On
+    - DetachDelete
+    - Call
+    - Yield
+    - Unwind
+    - Case (When/Then/Else) End
+    - CreateIndexOn
+    - UsingIndexOn
+    - DropIndexOn
+    - CreateConstraintOn
+    - DropConstraintOn
+    - Assert
+    - AssertIsUnique
+
+### Operators
+### Predicates/Conditions
+### Performance
+    - Profile
+    - Explain
--- builder.go
+++ builder.go
@@ -27,46 +27,6 @@ import (
 // [SET|DELETE|REMOVE|FOREACH]*
 // [RETURN [ORDER BY] [SKIP] [LIMIT]]
 
-// Query Clauses List
-// + Match
-// + Where
-// + With
-// + Return
-// + OrderBy
-// + Limit
-// - As
-// - OptionalMatch
-// - Skip
-// - Create
-// - CreateUnique
-// - Merge
-// - Set
-// - Delete
-// - Remove
-// - ForEach
-// - ReturnDistinct
-// - OrderByDesc
-// - Union
-// - On
-// - DetachDelete
-// - Call
-// - Yield
-// - Unwind
-// - Case (When/Then/Else) End
-// - CreateIndexOn
-// - UsingIndexOn
-// - DropIndexOn
-// - CreateConstraintOn
-// - DropConstraintOn
-// - Assert
-// - AssertIsUnique
-
-// Operators
-// Predicates/Conditions
-// Performance
-// 	- Profile
-// 	- Explain
-
 // Stringer ...
 type Stringer string
 
